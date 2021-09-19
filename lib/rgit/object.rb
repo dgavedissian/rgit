1e18fccc2af5b081c0a9ede3f69610b32b256d5d
David Avedissian <git@dga.dev>
2021-09-19 23:18:48 +0100

    Implement git log. Fix issue with kvlm_serialize

--- lib/rgit/object.rb
+++ lib/rgit/object.rb
@@ -37,6 +37,35 @@ module Rgit
     end
   end
 
+  class Commit < Object
+    def type
+      "commit"
+    end
+
+    attr_accessor :data
+
+    def parents
+      if @data.key?("parent")
+        parents = @data["parent"]
+        parents = [parents] unless parents.class == Array
+      else
+        nil
+      end
+    end
+
+    def message
+      @data.key?("") ? @data[""] : ""
+    end
+
+    def serialize
+      Rgit::kvlm_serialize(@data)
+    end
+
+    def deserialize(data)
+      @data = Rgit::kvlm_parse(data)
+    end
+  end
+
   class << self
     def object_create(type, data, repo)
       object_class = case type
@@ -67,17 +96,13 @@ module Rgit
         type = data[0...type_delim]
       
         # Read and validate object size.
-        size_delim = data.index('\0', type_delim)
-
-        # puts data[type_delim + 1...]
-        # puts "#{size_delim}"
-        # puts size_delim.type
-
+        size_delim = data.index("\0", type_delim)
         size = data[type_delim + 1...size_delim].to_i
         raise "Malformed object #{sha}: bad length" if size != data.length - size_delim - 1
 
-        object = create_object(type, data[size_delim + 1...])
-        raise "Unknown type #{type} for object #{sha}" if object == nil
+        object = object_create(type, data[size_delim + 1...], repo)
+        raise "Unknown type #{type} for object #{sha}" if object.nil?
+        return object
       end
 
       nil
@@ -88,8 +113,6 @@ module Rgit
       data = "#{object.type} #{data.length}\0" + data 
       sha = Digest::SHA1.hexdigest data
 
-      # print data
-
       if update_repo
         object.repo.file("objects", sha[0...2], sha[2...], mode: "wb", mkdir: true) do |f|
           f.write(Zlib::Deflate.deflate(data))
@@ -97,6 +120,60 @@ module Rgit
       end
 
       sha
+    end
+
+    def kvlm_parse(data, start=0, dict=nil)
+      dict = {} if dict.nil?
+
+      space = data.index(" ", start)
+      newline = data.index("\n", start)
+
+      # If space appears before newline, we have a keyword.
+
+      # Base Case: If newline appears first (or no space at all), we assume a blank line.
+      # A blank line means the remainder of the data is the message.
+      if space.nil? || newline < space
+        raise "Expected the next character to be a newline" unless newline == start
+        dict[""] = data[start + 1...]
+        return dict
+      end
+
+      # Recursive case: Read a key-value pair and recurse for the next.
+      key = data[start...space]
+      data_end = start
+      # Loop until we find a newline not followed by a space.
+      loop do
+        data_end = data.index("\n", data_end + 1)
+        break if data[data_end + 1] != " "
+      end
+      # Grab the value, making sure to drop the leading space on continuation lines.
+      value = data[space + 1...data_end].gsub("\n ", "\n")
+      # Append to the dictionary. This works by adding the empty string to the dict
+      # if a given key doesnt exist, then appending 'value' to dict[key] (which at this
+      # point is guaranteed to exist)
+      (dict[key] ||= "") << value
+      return kvlm_parse(data, start=data_end + 1, dict=dict)
+    end
+
+    def kvlm_serialize(kvlm)
+      out = ""
+
+      # Serialize fields.
+      kvlm.keys.each do |k|
+        # Skip the message itself.
+        next if k == ""
+
+        value = kvlm[k]
+        value = [value] unless value.class == Array
+
+        value.each do |v|
+          out << "#{k} #{v.gsub("\n", "\n ")}\n"
+        end
+      end
+
+      # Serialize the message.
+      message = kvlm[""]
+      out << "\n#{message}"
     end
   end
 end
